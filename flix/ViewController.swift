e5729f1f5119bec48240c28542dae1e6fe122d70
Mayur Barge <bargemayur05@gmail.com>
2019-02-01 22:06:09 -0800

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Mayur Barge on 2/1/19.
+//  Copyright © 2019 Mayur Barge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
